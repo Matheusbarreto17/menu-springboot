698553f822c2a81a4a2d72ce1063ef00298f821f
Matheusbarreto17 <matheus.barreto.pitta@gmail.com>
2025-10-21 16:19:03 +0100

    Primeiro commit - projeto menu

---
+++ src/main/java/com/wmdigital/menu_springboot/MenuSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wmdigital.menu_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmdigital/menu_springboot/MenuSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmdigital.menu_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
